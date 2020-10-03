0f2f4380e6f24d47341165e3134089f3a762c27c
anirudhseth <anirudhseth14@gmail.com>
2020-10-03 18:06:20 +0200

    gradcam v1

--- explanation_models.py
+++ explanation_models.py
@@ -12,7 +12,15 @@ class gradcam():
         inputs = [model.inputs],
         outputs = [model.get_layer(layer_name).output, model.output])
     
-    def computeHeatmap(self,img,index):
+    def get_heatmap(self,img,index):
+        with tf.GradientTape() as tape:
+            inputs = tf.cast(img, tf.float32)
+            (layer_output, class_prediction) = self.gradcamModel(inputs) # score of class c , y^c after softmax
+            # TODO : compare score before and after softmax ref : https://eli5.readthedocs.io/en/latest/tutorials/keras-image-classifiers.html#choosing-the-target-class-target-prediction
+            loss = class_prediction[:,index]  # 
+
+        gradients = tape.gradient(loss, layer_output) # gradient of y^c wrt to a^k of the covolution layer given as input
+        # TODO : global average pool to get ack
         
 
 class guided_backprop():
