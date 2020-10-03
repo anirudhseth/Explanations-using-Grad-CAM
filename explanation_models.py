fb5c491fb707894e9ee70fe1d0c720d94b04ec45
anirudhseth <anirudhseth14@gmail.com>
2020-10-03 17:46:14 +0200

    gradcam

--- explanation_models.py
+++ explanation_models.py
@@ -1,9 +1,19 @@
 import tensorflow as tf 
 
-class gradcam():
 
-    def __init__(self):
-        print('test')
+class gradcam():
+    '''
+    Implimentation of GradCAM : Grad-CAM: Visual Explanations from Deep Networks via Gradient-based Localization
+    url: https://arxiv.org/abs/1610.02391
+    '''
+
+    def __init__(self,model,layer_name):
+        self.gradcamModel= tf.keras.Model(
+        inputs = [model.inputs],
+        outputs = [model.get_layer(layer_name).output, model.output])
+    
+    def computeHeatmap(self,img,index):
+        
 
 class guided_backprop():
 
