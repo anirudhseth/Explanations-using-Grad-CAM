1d07c7e69c9234510f9ff2650fbb194dc0669e7b
anirudhseth <anirudhseth14@gmail.com>
2020-10-10 23:02:48 +0200

    gradcam tested

--- explanation_models.py
+++ explanation_models.py
@@ -14,39 +14,51 @@ class gradcam():
         outputs = [model.get_layer(layer_name).output, model.output])
         self.input_dim=input_dim
     
-    def get_heatmap(self,img,index):
+    def get_heatmap(self,img,index=None):
         # TODO : image with batch dimension or without, better to add when getting predictions 
         with tf.GradientTape() as tape:
             inputs = tf.cast(img, tf.float32)  # use keras preprocessing instead before the function
             (layer_output, class_prediction) = self.gradcamModel(inputs) # score of class c , y^c after softmax  1, 14, 14, 512 and 1, 1000 , have batch dimension
             # TODO : compare score before and after softmax ref : https://eli5.readthedocs.io/en/latest/tutorials/keras-image-classifiers.html#choosing-the-target-class-target-prediction
+            if(index==None):
+                index=tf.argmax(class_prediction[0])
+            tape.watch(layer_output)
             loss = class_prediction[:,index]  # 
-            gradients = tape.gradient(loss, layer_output) # gradient of y^c wrt to a^k of the covolution layer given as input
+        gradients = tape.gradient(loss, layer_output) # gradient of y^c wrt to a^k of the covolution layer given as input
         # [1, 14, 14, 512]) same dimensions as the output of the last layer
-        layer_output = layer_output[0]
-        gradients=gradients[0] # remove batch dimension [14, 14, 512]
+        layer_output = layer_output.numpy()[0]
+        # gradients=gradients[0] # remove batch dimension [14, 14, 512]
 
-        normalized_gradients = tf.divide(gradients, tf.sqrt(tf.reduce_mean(tf.square(gradients))) + tf.keras.backend.epsilon())
+        # normalized_gradients = tf.divide(gradients, tf.sqrt(tf.reduce_mean(tf.square(gradients))) + tf.keras.backend.epsilon())
         # check directly normalizing https://donghwa-kim.github.io/backpropa_CNN.html
         
-        neuron_importance_weights=tf.reduce_mean(normalized_gradients,axis=(0,1)) # ack in the paper  512
-
-        gradcam = tf.reduce_sum(tf.multiply(neuron_importance_weights, layer_output), axis=-1)  #14 x 14
-
+        # neuron_importance_weights=tf.reduce_mean(normalized_gradients,axis=(0,1)) # ack in the paper  512
+        neuron_importance_weights=tf.reduce_mean(gradients, axis=(0, 1, 2)).numpy()
+        # gradcam_test = tf.reduce_sum(tf.multiply(neuron_importance_weights, layer_output), axis=-1)  #14 x 14
+        
+        for i in range(neuron_importance_weights.shape[-1]):
+          layer_output[:, :, i] *= neuron_importance_weights[i]
+        gradcam = np.mean(layer_output, axis=-1)
         gradcam = tf.keras.activations.relu(gradcam)
         gradcam = gradcam/np.max(gradcam)   # check without this 
-        gradcam = cv2.resize(np.float32(gradcam), self.input_dim,interpolation=cv2.INTER_LINEAR)  #upscaling cv2 is buggy here
+        # gradcam = cv2.resize(np.float32(gradcam), self.input_dim,interpolation=cv2.INTER_LINEAR)  #upscaling cv2 is buggy here
         return gradcam
 
-    def overlay_heatmap(self,img,heatmap):
+    def overlay_heatmap(self,img,heatmap_lower_dim):
         '''
         returns the heatmap with the applied colormap and the overlayed image 
         '''
-        heatmap3d = np.expand_dims(heatmap, axis=2)
-        heatmap3d = np.tile(heatmap3d, [1,1,3])
-        heatmap3d_colormap= cv2.applyColorMap(np.uint8(255*heatmap3d),cv2.COLORMAP_HOT)
-        fused_img = cv2.addWeighted(img,0.7,heatmap3d_colormap,0.3,0)
-        return heatmap3d_colormap,fused_img
+        heatmap=np.squeeze(heatmap_lower_dim)
+        heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+        heatmap = (heatmap*255).astype("uint8")
+        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+        superimposed_img = heatmap * 0.4 + img
+        superimposed_img = np.clip(superimposed_img,0,255).astype("uint8")
+        # superimposed_img = cv2.cvtColor(superimposed_img, cv2.COLOR_BGR2RGB)
+
+        return heatmap,superimposed_img  
+
+
 class guided_backprop():
 
     def __init__(self):
--- image_utils.py
+++ image_utils.py
@@ -3,11 +3,19 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing import image
-
+import json
 class imagenet_utils():
 
     def load_test_img():
-        orig_img = image.load_img('data/dog.jpg',target_size=(224,224))
+
+        # !wget https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json
+        # f = open("imagenet_class_index.json", "r")
+        # class_name_dict = json.load(f)
+        # print(class_name_dict['202'])
+        # !wget -O dog.jpg https://i.insider.com/536aa78069bedddb13c60c3a?width=600&format=jpeg&auto=webp
+        dog_img='dog.jpg'
+        orig_img = image.load_img(dog_img,target_size=(224,224))
+        # orig_img = image.load_img('data/dog.jpg',target_size=(224,224))
         orig_img = np.asarray(orig_img)
         img = np.expand_dims(orig_img, axis=0)
         return orig_img,img
