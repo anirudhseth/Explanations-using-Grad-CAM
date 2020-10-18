2685c54d461ea34e0ffe7d1f4ab87f00d23596d8
anirudhseth <anirudhseth14@gmail.com>
2020-10-18 20:15:28 +0200

    gradcampp init

--- explanation_models.py
+++ explanation_models.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import cv2
 from tensorflow import keras
+
 class gradcam():
     '''
     Implimentation of GradCAM : Grad-CAM: Visual Explanations from Deep Networks via Gradient-based Localization
@@ -17,48 +18,21 @@ class gradcam():
     def get_heatmap(self,img,index=None):
  
         with tf.GradientTape() as tape:
-           
-           
-            (layer_output, class_prediction) = self.gradcamModel(img) # score of class c , y^c after softmax  1, 14, 14, 512 and 1, 1000 , have batch dimension
-            # TODO : compare score before and after softmax ref : https://eli5.readthedocs.io/en/latest/tutorials/keras-image-classifiers.html#choosing-the-target-class-target-prediction
+            (layer_output, class_prediction) = self.gradcamModel(img) 
             if(index==None):
-                index=tf.argmax(class_prediction[0])
+                index=tf.argmax(class_prediction[0])  # use the index with best score if none provided
             tape.watch(layer_output)
             loss = class_prediction[:,index]  # 
         gradients = tape.gradient(loss, layer_output) # gradient of y^c wrt to a^k of the covolution layer given as input
         # [1, 14, 14, 512]) same dimensions as the output of the last layer
         layer_output = layer_output.numpy()[0]
-        # gradients=gradients[0] # remove batch dimension [14, 14, 512]
-
-        # normalized_gradients = tf.divide(gradients, tf.sqrt(tf.reduce_mean(tf.square(gradients))) + tf.keras.backend.epsilon())
-        # check directly normalizing https://donghwa-kim.github.io/backpropa_CNN.html
-        
-        # neuron_importance_weights=tf.reduce_mean(normalized_gradients,axis=(0,1)) # ack in the paper  512
         neuron_importance_weights=tf.reduce_mean(gradients, axis=(0, 1, 2)).numpy()
-        # gradcam_test = tf.reduce_sum(tf.multiply(neuron_importance_weights, layer_output), axis=-1)  #14 x 14
-        
         for i in range(neuron_importance_weights.shape[-1]):
           layer_output[:, :, i] *= neuron_importance_weights[i]
         gradcam = np.mean(layer_output, axis=-1)
         gradcam = tf.keras.activations.relu(gradcam)
-        gradcam = gradcam/np.max(gradcam)   # check without this 
-        # gradcam = cv2.resize(np.float32(gradcam), self.input_dim,interpolation=cv2.INTER_LINEAR)  #upscaling cv2 is buggy here
+        gradcam = gradcam/np.max(gradcam)    
         return gradcam
-
-    def overlay_heatmap(self,img,heatmap_lower_dim):
-        '''
-        returns the heatmap with the applied colormap and the overlayed image 
-        '''
-        img=keras.preprocessing.image.img_to_array(img)
-        heatmap=np.squeeze(heatmap_lower_dim)
-        heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
-        heatmap = (heatmap*255).astype("uint8")
-        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-        fused_img = heatmap * 0.4 + img
-        fused_img = np.clip(fused_img,0,255).astype("uint8")
-        # fused_img = cv2.cvtColor(fused_img, cv2.COLOR_BGR2RGB)
-
-        return heatmap,fused_img  
 
 class guided_backprop():
 
@@ -67,7 +41,8 @@ class guided_backprop():
 
 class gradcam_robust():
 
-    def __init__(self,model,layers,input_dim):
+    def __init__(self,model,layers,input_dim,gradcam_variant):
+        self.gradcam_variant=gradcam_variant
         self.layers=layers
         self.model=model
         self.input_dim=input_dim
@@ -75,28 +50,15 @@ class gradcam_robust():
     def get_heatmap(self,img,index=None):
         robust_heatmap=[]
         for layer in self.layers:
-            g= gradcam(self.model,layer,self.input_dim)
+            if self.gradcam_variant=='GradCAM':
+                g= gradcam(self.model,layer,self.input_dim)
+            elif self.gradcam_variant=='GradCAMpp':
+                g= gradcam_plusplus(self.model,layer,self.input_dim)
             heatmap=g.get_heatmap(img)
             robust_heatmap.append(heatmap)
             del g
         return robust_heatmap
-        # g= gradcam(model,layer_name,input_dim)
-        # heatmap=g.get_heatmap(img)
 
-    def overlay_heatmap(self,img,heatmap_lower_dim):
-        '''
-        returns the heatmap with the applied colormap and the overlayed image 
-        '''
-        img=keras.preprocessing.image.img_to_array(img)
-        heatmap=np.squeeze(heatmap_lower_dim)
-        heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
-        heatmap = (heatmap*255).astype("uint8")
-        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-        fused_img = heatmap * 0.4 + img
-        fused_img = np.clip(fused_img,0,255).astype("uint8")
-        # fused_img = cv2.cvtColor(fused_img, cv2.COLOR_BGR2RGB)
-
-        return heatmap,fused_img 
 
 class gradcam_plusplus():
     '''
@@ -118,23 +80,26 @@ class gradcam_plusplus():
                     if(index==None):
                         index=tf.argmax(class_prediction[0])
                     loss = class_prediction[:,index]  
-                    gradients1 = tape.gradient(loss, layer_output) 
-                gradients2 = tape.gradient(gradients1, layer_output)
-            gradients3 = tape.gradient(gradients2, layer_output)
+                    gradients1 = tape3.gradient(loss, layer_output) 
+                gradients2 = tape2.gradient(gradients1, layer_output)
+            gradients3 = tape1.gradient(gradients2, layer_output)
 
-        return gradcam
+        global_sum = np.sum(layer_output, axis=(0, 1, 2))
 
-    def overlay_heatmap(self,img,heatmap_lower_dim):
-        '''
-        returns the heatmap with the applied colormap and the overlayed image 
-        '''
-        img=keras.preprocessing.image.img_to_array(img)
-        heatmap=np.squeeze(heatmap_lower_dim)
-        heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
-        heatmap = (heatmap*255).astype("uint8")
-        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-        fused_img = heatmap * 0.4 + img
-        fused_img = np.clip(fused_img,0,255).astype("uint8")
-        # fused_img = cv2.cvtColor(fused_img, cv2.COLOR_BGR2RGB)
+        neuron_importance_weights_num = gradients2[0]
+        neuron_importance_weights_denom = 2*gradients2[0] + gradients3[0]*global_sum
+        neuron_importance_weights_denom = np.where(neuron_importance_weights_denom != 0.0, neuron_importance_weights_denom, 1e-10)
+        
+        neuron_importance_weights = neuron_importance_weights_num/neuron_importance_weights_denom
+        Z = np.sum(neuron_importance_weights, axis=(0,1))
+        neuron_importance_weights /= Z
 
-        return heatmap,fused_img  
+        weights = tf.keras.activations.relu(gradients1[0]) 
+        weights = np.sum(weights*neuron_importance_weights, axis=(0,1))
+        gradcampp = np.sum(weights*layer_output[0], axis=2)
+
+        gradcampp = tf.keras.activations.relu(gradcampp)
+        gradcampp=gradcampp/np.max(gradcampp) 
+
+        return gradcampp
+  
--- image_utils.py
+++ image_utils.py
@@ -18,4 +18,19 @@ class imagenet_utils():
         preprocessed_input=preprocess_input(array)
         return img,preprocessed_input
 
+    def overlay_heatmap(img,heatmap_lower_dim):
+        '''
+        returns the heatmap with the applied colormap and the overlayed image 
+        '''
+        img=keras.preprocessing.image.img_to_array(img)
+        heatmap=np.squeeze(heatmap_lower_dim)
+        heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+        heatmap = (heatmap*255).astype("uint8")
+        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+        fused_img = heatmap * 0.4 + img
+        fused_img = np.clip(fused_img,0,255).astype("uint8")
+        # fused_img = cv2.cvtColor(fused_img, cv2.COLOR_BGR2RGB)
+
+        return heatmap,fused_img  
+
         
